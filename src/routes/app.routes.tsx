62e9ea8a8f36d1e2c88413fd386664abf78c1086
christianfelipecarvalho <christianfc001@gmail.com>
2024-10-06 22:47:59 -0300

    Ajustando estilos, tela order e rotas

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,36 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+
+import Dashboard from '../pages/Dashboard';
+import Order from '../pages/Order';
+
+export type StackPramsList = {
+  Dashboard: undefined;
+  Order: {
+    number: number | string;
+    order_id: string;
+  };
+};
+
+const Stack = createNativeStackNavigator<StackPramsList>();
+
+function AppRoutes(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="Dashboard" 
+        component={Dashboard} 
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="Order"
+        component={Order}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  )
+}
+
+export default AppRoutes;
+
